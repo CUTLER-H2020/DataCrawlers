150b6183ef08e4e7367f6b418a7e46924279c94a
Karypidis Paris Alexandros <karypidisparisalexandros@gmail.com>
2019-03-28 13:54:14 +0200

    Finilizing immoscoop crawler

--- Economic/immoscoop.be/myenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/user/Desktop/TEMP/DataCrawlers/Economic/immoscoop.be/myenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
