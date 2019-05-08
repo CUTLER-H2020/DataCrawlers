8edefb21e57610d1df06dead85be8302e4b72ae5
hassanmehmud <hmehmood@student.oulu.fi>
2019-05-08 15:13:05 +0300

    Crawler/scraper for D_ID: CORK_ENV_MET_W_DAILY

---
+++ Environmental/CORK_ENV_MET_W_DAILY.py
@@ -0,0 +1,29 @@
+# -*- coding utf-8 -*-
+""" -This Crawler+Scraper requests the URL (data.gov.ie - Met Éireann)) and fetches the content of the file containing historical readings of weather condition"""
+""" -The time is reported in UTC which is then converted to ISO 8601 format with UTC offset"""
+""" -The available data is formatted, and   it is stored in .CSV format with unique name"""
+
+import uuid
+import pandas as pd
+
+__author__ = "Hassan Mehmood"
+__email__ = "hassan.mehmood@oulu.fi"
+__origin__ = "UbiComp - University of Oulu"
+
+def historicWeatherInfo():
+
+    """This function fetches the data from the source and after cleaning stores it to .CSV file"""
+    path = "" # PATH to storage directory
+    uFileName = str(uuid.uuid4())
+    filname = path + uFileName + ".csv"
+    url = 'https://cli.fusio.net/cli/climate_data/webdata/hly1075.csv'
+    df = pd.read_fwf(url)
+    df_1 = df[14:] #Removes unnecessary rows contains duration of data etc.
+    df_final = df_1["Station Name: ROCHES POINT"].str.split(",", n=14, expand=True) # As data is ; separated, additional formatting is done to allocate observation to their corresponding columns
+    df_final.columns = ['DateTime', 'indicator_rain', 'rain', 'indicator_temp', 'temp', 'indicator_wetb', 'wetb', 'dewpt',
+                   'vappr', 'rhum', 'msl', 'indicator_wdsp', 'wdsp', 'indicator_wddir', 'wddir']
+    df_final['DateTime'] = pd.to_datetime(df_final['DateTime']) # Transforms available time information in string to datetime format
+    df_final['DateTime'] = df_final["DateTime"].dt.strftime("%Y-%m-%dT%H:%M+00") # Transforms available time information to ISO 8601
+    df_final.to_csv(filname, index = False)
+
+historicWeatherInfo()
