a4d2dd6015105532b576ba1ea52238e4ddd95939
Stelios Kitziris <skitziris@draxis.gr>
2019-08-08 13:07:09 +0300

    add error

--- Environmental/create_all_topics.js
+++ Environmental/create_all_topics.js
@@ -2,13 +2,15 @@
 
 const kafka_topics = require('./lib/Kafka/KafkaTopics.js');
 
-kafka_topics.createAll(function (err, result) {
-    if (result.length == 0) {
-        console.log("All topics created successfully!");
-    } else {
-        console.log("Failed to create the topics!");
-        console.error(result);
-    }
+kafka_topics.createAll(function(err, result) {
+  if (err) return console.log(err);
 
-    kafka_topics.client.close();
+  if (result.length == 0) {
+    console.log('All topics created successfully!');
+  } else {
+    console.log('Failed to create the topics!');
+    console.error(result);
+  }
+
+  kafka_topics.client.close();
 });
