80e8a4b6c7e1394b70bd86fdabfca7b76fb8d54f
macordu <marta.cortes@oulu.fi>
2019-11-05 14:06:08 +0200

    Update CORK_ENV_ MARINEINSTITUTE2_DAILY_3.py

--- Environmental/CORK_ENV_ MARINEINSTITUTE2_DAILY_3.py
+++ Environmental/CORK_ENV_ MARINEINSTITUTE2_DAILY_3.py
@@ -1,4 +1,15 @@
 # -*- coding utf-8 -*-
+""" This code is open-sourced software licensed under the MIT license""" 
+""" Copyright  2019 Hassan Mehmood, UbiComp - University of Oulu""" 
+""" Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+""" 
+""" 
+DISCLAIMER
+This code is used to crawl/parse data from several files from Irish Marine institute (https://erddap.marine.ie). By downloading this code, you agree to contact the corresponding data provider and verify you are allowed to use (including, but not limited, crawl/parse/download/store/process) all data obtained from the data source.
+""" 
+
 """ -This Crawler+Scraper requests the URL (Marine institute Ocean Forecast observations) and fetches the HTML source of the target page"""
 """ -The recieved page source is then parsed for available data"""
 """ -The time is reported in UTC with Z-Zulu extension which is then converted to UTC offset"""
