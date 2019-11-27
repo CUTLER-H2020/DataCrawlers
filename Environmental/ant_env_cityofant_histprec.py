2bcd81f71f2c5e053245c6bdab9e9bd23c9a307a
macordu <marta.cortes@oulu.fi>
2019-11-27 15:59:00 +0200

    kafka + kafka errors+ exception loggin + cleaning

--- Environmental/ant_env_cityofant_histprec.py
+++ Environmental/ant_env_cityofant_histprec.py
@@ -60,84 +60,135 @@ class ant_env_cityofant_histprec (object):
 
 	def parse_files(self):
 
-		#first, parse sensor locations
-		xlflocationsname = l_temp_path+code+'/'+xlflocations
-		xl_l = pd.ExcelFile(xlflocationsname)
-		df_stations = xl_l.parse(names_sheetn)
+		try: 
+			#first, parse sensor locations
+			xlflocationsname = l_temp_path+code+'/'+xlflocations
+			xl_l = pd.ExcelFile(xlflocationsname)
+			df_stations = xl_l.parse(names_sheetn)
+
+		except Exception as e:
+			
+			self.producer("ANT_ENV_CITYOFANT_HISTPREC_DATA_ERROR",'data source not found or cannot be open',e)
+			return False
 		#df_stations.rename(columns={df_stations.columns[3]:'Longitude',df_stations.columns[4]:'Latitude', 'NAAM':'Name'},inplace=True)
 		
-		#second, parse data
-		for fileName in xlfnames:
-			xlfname = l_temp_path+code+'/'+fileName#
-			xl = pd.ExcelFile(xlfname)
-			print ('opening file '+fileName)
-			#df_stations = xl.parse(names_sheetn)
-			#print (df_stations.columns.tolist)
-			#print (df_stations.index)
-			#df_stations.rename(columns={df_stations.columns[3]:'Longitude',df_stations.columns[4]:'Latitude', 'NAAM':'Name'},inplace=True)
-			#print (df_stations.columns.tolist)
-			#print (df_stations.index)
-			
-			df_clean_data = xl.parse (clean_data_sheetn)#,parse_cols=[0,2,4,6,8,10,12])
-			#print (df_clean_data.columns.tolist)
-			#print (df_clean_data.index)
-			df_clean_data.reset_index(inplace = True)
-			#print (df_clean_data.columns.tolist)
-			df_clean_data.rename(columns={df_clean_data.columns[0]:'DateTime'},inplace=True)
-			print (df_clean_data.columns.tolist)
-			df_temp = pd.DataFrame()
-			for column in df_clean_data:
+		try:
+			df = pd.DataFrame()
+			#second, parse data
+			for fileName in xlfnames:
+				try:
+					xlfname = l_temp_path+code+'/'+fileName#
+					xl = pd.ExcelFile(xlfname)
+					print ('opening file '+fileName)
+				except Exception as e:
+					self.producer("ANT_ENV_CITYOFANT_HISTPREC_DATA_ERROR",'data source not found or cannot be open',e)
+					return False
+				df_clean_data = xl.parse (clean_data_sheetn)#,parse_cols=[0,2,4,6,8,10,12])
+				#print (df_clean_data.columns.tolist)
+				#print (df_clean_data.index)
+				df_clean_data.reset_index(inplace = True)
+				#print (df_clean_data.columns.tolist)
+				df_clean_data.rename(columns={df_clean_data.columns[0]:'DateTime'},inplace=True)
+				print (df_clean_data.columns.tolist)
+				df_temp = pd.DataFrame()
+				for column in df_clean_data:
 
-				#Directory name by code/codenumber
-				if column != 'DateTime' and column.find('.')!=-1:
-					print ('on column '+column)
-					name =column.split(".")[0]
-					print ('on column name '+name)
-					#new datagram with the station values
-					#df_temp = df_clean_data [df_clean_data.columns[0], column]#.to_csv(fullname, mode='w', encoding='utf-8', index=False)
-					df_temp = df_clean_data [['DateTime',column]].copy()
-					#df_temp [name]= df_clean_data [column]
-					df_temp.rename(columns={column:'Rainfall'},inplace=True)
-					#Format time into ISO 8601
-					df_temp['DateTime'] = pd.to_datetime(df_temp['DateTime'], format='%Y-%m-%d%H:%M:%S').dt.strftime('%Y-%m-%dT%H:%M+01')
-					#antwerp = pytz.timezone('Europe/Brussels')
-					#df_temp['DateTime'] = pst.localize(pd.to_datetime(df_temp['DateTime'], format='%Y-%m-%d%H:%M:%S')).isoformat()#.dt.strftime('%Y-%m-%dT%H:%M+01')
-					#df_temp['DateTimeLoc'] = pd.to_datetime(df_temp['DateTime'], format='%Y-%m-%d%H:%M:%S').tz_convert(antwerp)
-					#df_temp['DateTimeLoc'] = pd.to_datetime(df_temp['DateTime'], format='%Y-%m-%d%H:%M:%S').dt.tz_localize(antwerp, ambiguous=True)
-					#get the NR of the station from the name
-					#s = ''.join(x for x in name if x.isdigit())
-					#print ('column NR '+s)
-					#get the values of the station
-					yl = df_stations.loc[df_stations['NR'] == name,'Y'].item()
-					xl = df_stations.loc[df_stations['NR'] == name,'X'].item()
-					print ('x is '+str(xl))
-					print ('y is '+str(yl))
-					df_temp['Y'] = yl#df_stations.loc[df_stations['NR'] == name,'Y']#  int(s),'Latitude']
-					df_temp['X'] = xl#df_stations.loc[df_stations['NR'] == name,'X']#int(s),'Longitude']
-					#TODO Format X Y into Latitude Longitude if needed
-					#df_temp['Latitude'] = df_stations.loc[df_stations['NR'] == name,'Y'].item()#  int(s),'Latitude']
-					#df_temp['Longitude'] = df_stations.loc[df_stations['NR'] == name,'X'].item()#int(s),'Longitude']
-					df_temp['Location'] = df_stations.loc[df_stations['NR'] == name,'LOCATIOn'].item()#int(s),'Name']
-					df_temp['NR'] = df_stations.loc[df_stations['NR'] == name,'NR'].item()
-					#create folder/file structure
-					outerdir = l_final_path+code
-					if not os.path.exists(outerdir):
-						os.mkdir(outerdir)
-					outdir = outerdir+'/'+code+'_'+name
-					if not os.path.exists(outdir):
-						os.mkdir(outdir)
-					#df = df.append(df_tmp, ignore_index=True)
-			        #Write to the csv file. Note, put this out of the loop to write all the sheets in same csv file
-					csvfile = str(uuid.uuid4()) + ".csv"#sheet+'.csv'
-					print ('writing to folder '+code+'_'+name)
-					fullname = os.path.join(outdir, csvfile)
-					df_temp.rename(columns={'NR':'Sensor code'},inplace=True)
-					df_temp.to_csv(fullname, mode='a', encoding='utf-8-sig', index=False)
-	def producer(self):
-		""" This function sends data to kafka bus"""
+					#Directory name by code/codenumber
+					if column != 'DateTime' and column.find('.')!=-1:
+						print ('on column '+column)
+						name =column.split(".")[0]
+						print ('on column name '+name)
+						#new datagram with the station values
+						#df_temp = df_clean_data [df_clean_data.columns[0], column]#.to_csv(fullname, mode='w', encoding='utf-8', index=False)
+						df_temp = df_clean_data [['DateTime',column]].copy()
+						#df_temp [name]= df_clean_data [column]
+						df_temp.rename(columns={column:'Rainfall'},inplace=True)
+						#Format time into ISO 8601
+						df_temp['DateTime'] = pd.to_datetime(df_temp['DateTime'], format='%Y-%m-%d%H:%M:%S').dt.strftime('%Y-%m-%dT%H:%M+01')
+						#antwerp = pytz.timezone('Europe/Brussels')
+						#df_temp['DateTime'] = pst.localize(pd.to_datetime(df_temp['DateTime'], format='%Y-%m-%d%H:%M:%S')).isoformat()#.dt.strftime('%Y-%m-%dT%H:%M+01')
+						#df_temp['DateTimeLoc'] = pd.to_datetime(df_temp['DateTime'], format='%Y-%m-%d%H:%M:%S').tz_convert(antwerp)
+						#df_temp['DateTimeLoc'] = pd.to_datetime(df_temp['DateTime'], format='%Y-%m-%d%H:%M:%S').dt.tz_localize(antwerp, ambiguous=True)
+						#get the NR of the station from the name
+						#s = ''.join(x for x in name if x.isdigit())
+						#print ('column NR '+s)
+						#get the values of the station
+						yl = df_stations.loc[df_stations['NR'] == name,'Y'].item()
+						xl = df_stations.loc[df_stations['NR'] == name,'X'].item()
+						print ('x is '+str(xl))
+						print ('y is '+str(yl))
+						df_temp['Y'] = yl#df_stations.loc[df_stations['NR'] == name,'Y']#  int(s),'Latitude']
+						df_temp['X'] = xl#df_stations.loc[df_stations['NR'] == name,'X']#int(s),'Longitude']
+						#TODO Format X Y into Latitude Longitude if needed
+						#df_temp['Latitude'] = df_stations.loc[df_stations['NR'] == name,'Y'].item()#  int(s),'Latitude']
+						#df_temp['Longitude'] = df_stations.loc[df_stations['NR'] == name,'X'].item()#int(s),'Longitude']
+						df_temp['Location'] = df_stations.loc[df_stations['NR'] == name,'LOCATIOn'].item()#int(s),'Name']
+						df_temp['NR'] = df_stations.loc[df_stations['NR'] == name,'NR'].item()
+						
+						##comment if one folder/file per numeric code
+						df = df.append(df_temp, ignore_index=True)
+						
+						## One folder/file per numeric code
+						"""
+						try:
+
+							#Note, put this out of the loop to write all the sheets in same csv file
+							#create folder/file structure
+							outerdir = l_final_path+code
+							if not os.path.exists(outerdir):
+								os.mkdir(outerdir)
+							#CODE FOR ONE STATION ONE FOLDER<-Should go in the loop
+							outdir = outerdir+'/'+code+'_'+name
+							if not os.path.exists(outdir):
+								os.mkdir(outdir)
+					        	#Write to the csv file. 
+							csvfile = str(uuid.uuid4()) + ".csv"#sheet+'.csv'
+							fullname = os.path.join(outdir, csvfile)
+							#CODE FOR ONE STATION ONE FOLDER<-Should go in the loop
+							df_temp.rename(columns={'NR':'Sensor_id'},inplace=True)
+							df_temp.to_csv(fullname, mode='a', encoding='utf-8-sig', index=False)
+						except Exception as e:
+							
+							self.producer("ANT_ENV_CITYOFANT_HISTPREC_DATA_ERROR",'cannot store data in csv file',e)
+							return False
+						"""
+		
+					##comment if one folder/file per numeric code
+			df.rename(columns={'NR':'Sensor_id'},inplace=True)
+
+		except Exception as e:
+			
+			self.producer("ANT_ENV_CITYOFANT_HISTPREC_DATA_ERROR",'data source format is not as expected',e)
+
+			return False
+		##all the data in the same folder/file
+		try:
+
+			#Note, put this out of the loop to write all the sheets in same csv file
+			#create folder/file structure
+			outerdir = l_final_path+code
+			if not os.path.exists(outerdir):
+				os.mkdir(outerdir)
+	        	#Write to the csv file. 
+			csvfile = str(uuid.uuid4()) + ".csv"#sheet+'.csv'
+			print ('writing to folder '+code+'_'+name)
+			fullname = os.path.join(outdir, csvfile)
+			
+			df.to_csv(fullname, mode='a', encoding='utf-8-sig', index=False)
+		except Exception as e:
+			
+			self.producer("ANT_ENV_CITYOFANT_HISTPREC_DATA_ERROR",'cannot store data in csv file')
+			return False
+		
+		return True	
+			
+	def producer(self,topic,msg,e=None):
+	""" This function sends data to kafka bus"""
 		producer = KafkaProducer(bootstrap_servers=['HOST_IP'], api_version=(2, 2, 1))
-		topic = "ANT_ENV_CITYOFANT_HISTPREC_DATA_INGESTION"
-		producer.send(topic, b'Historic precipitation data for antwerp ingested to HDFS').get(timeout=30)
+		msg_b = str.encode(msg)
+		producer.send(topic, msg_b).get(timeout=30)
+		if (e):
+			logging.exception('exception happened')	
 
 if __name__ == '__main__':
 	a = ant_env_cityofant_histprec()
